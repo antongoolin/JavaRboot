1bec4094b7398becc2cf8231b470cc0942d4d3ff
antongoolin <71603302+antongoolin@users.noreply.github.com>
2020-12-14 12:42:50 +0300

    Revert "homework "LinkedLists" from lesson 6"

--- homework6_lists/src/main/java/ru/sbrf/MyQueue.java
+++
@@ -1,7 +0,0 @@
-package ru.sbrf;
-
-public interface MyQueue<T extends Comparable<T>> extends MyList<T >{
-    // Отдать первый в очереди элемент и его из очереди удалить
-    // Если нет - отдаем Null
-    T poll();
-}
